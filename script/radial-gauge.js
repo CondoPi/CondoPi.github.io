13b4332c9abcdcd75490fb4898054bf9e97b6a1c
PoundKake <vance.christian@gmail.com>
2019-11-23 23:48:50 -0500

    Testing retrieval of json data from dataplicity.

--- script/radial-gauge.js
+++ script/radial-gauge.js
@@ -58,9 +58,9 @@ function updateGaugeVal() {
     // This is used for testing purposes.
     gauge.value = (Math.random() * (maxVal - minVal)) + minVal;
 
-    // fetch(dataplicityWormHoleAddress)
-    //     .then(res => res.json())
-    //     .then((out) => {
-    //         console.log('Output: ', out);
-    // }).catch(err => console.error(err));
+    fetch(dataplicityWormHoleAddress)
+        .then(res => res.json())
+        .then((out) => {
+            console.log('Output: ', out);
+    }).catch(err => console.error(err));
 }
